bb38cb6b06feb9b19f6a0efe1df7266e2cf8e1ba
Jay Prakash Kumar <jay.kasyap@live.com>
2020-07-18 20:31:25 +0530

    Added scss file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>        
+      <header className="App-header">    
+      <h1>Hello World!</h1>              
       </header>
     </div>
   );
